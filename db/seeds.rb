c2c7451be5a98b8d351da675153c3760cb6ebce9
pnb90 <phuocbui90@gmail.com>
2019-05-18 16:45:01 -0500

    made a good good seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,54 @@
-Restaurant.create([
-                  (name: "KFC"),
-                  (name: "Church's Chicken")
+Restaurant.create!([
+                  {name: "pHlour"},
+                  {name: "Crisp"},
+                  {name: "KFC"},
+                  {name: "Church's Chicken"}
                   ])
+
+User.create!([
+            { 
+              name: "Phuoc",
+              email: "phuoc@gmail.com",
+              password: "password"
+            },
+            {
+              name: "Mike",
+              email: "mike@gmail.com",
+              password: "password"
+            },
+            {
+              name: "Brye",
+              email: "brye@gmail.com",
+              password: "password"
+            },
+            {
+              name: "Mark",
+              email: "mark@gmail.com",
+              password: "password"
+            },
+            {
+              name: "Anjelica",
+              email: "anjelica@gmail.com",
+              password: "password"
+            },
+            {
+              name: "Victoria",
+              email: "victoria@gmail.com",
+              password: "password"
+            },
+            {
+              name: "Patty",
+              email: "patty@gmail.com",
+              password: "password" 
+            },
+            {
+              name: "Matt",
+              email: "matt@gmail.com",
+              password: "password"
+            },
+            {
+              name: "Leo",
+              email: "leo@gmail.com",
+              password: "password" 
+              }
+            ])
